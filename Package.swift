661872bc9a0985f6b6419c0b3313ef0b615970ec
Sergiu Todirascu <sergiu@getstoryteller.com>
2024-08-27 16:51:08 +0300

    Updating Storyteller GAM Module to 10.5.0

--- Package.swift
+++ Package.swift
@@ -3,21 +3,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "StorytellerGAMIntegration",
+    name: "storyteller-gam-integration",
     platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "StorytellerGAMIntegration",
-            targets: ["StorytellerGAMIntegration"]
+            targets: ["StorytellerGAMTarget"]
         ),
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .upToNextMajor(from: "11.0.0")),
-        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "10.4.0")
+        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "10.5.0")
     ],
     targets: [
         .binaryTarget(name: "StorytellerGAMIntegration",
-                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.4.0/StorytellerGAMIntegration.zip",
-                      checksum: "21d3bb3b50d8a7c01501f782dad63f3feaaf20bf65f904acd197ecc1c31c04f9")
+                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.5.0/StorytellerGAMIntegration.zip",
+                      checksum: "bd6f472c424d20349d153ffe36ac8fe0346f226444b7619d9eb0510ccc4a949a"),
+        .target(
+            name: "StorytellerGAMTarget",
+            dependencies: [
+                .target(name: "StorytellerGAMIntegration"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "StorytellerSDK", package: "storyteller-sdk-swift-package"),
+            ],
+            path: "Sources"
+        )
     ]
 )
