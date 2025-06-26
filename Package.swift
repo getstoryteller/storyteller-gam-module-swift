08f98f8e2259f821bcf6d1f0b093b8549a87230f
runner <runner@Mac-1750942423603.local>
2025-06-26 13:43:24 +0000

    Updating Storyteller GAM Module to 10.12.0

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .upToNextMajor(from: "11.0.0")),
-        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "10.11.1")
+        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "10.12.0")
     ],
     targets: [
         .binaryTarget(name: "StorytellerGAMIntegration",
-                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.11.1/StorytellerGAMIntegration.zip",
-                      checksum: "798a13be0af6b558838bd2c7326b32ea68498769d90b9159270c1f0edc2a067e"),
+                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/10.12.0/StorytellerGAMIntegration.zip",
+                      checksum: "028020561c1d7cb57fe6860e2e3ae0d505fe3da9ca83f910a75816db94038f28"),
         .target(
             name: "StorytellerGAMTarget",
             dependencies: [
