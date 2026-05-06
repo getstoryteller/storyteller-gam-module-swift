97c2674ae04bdfa88607c669de4ab0dc34cf57e0
Anka <runner@sat12-bq152-80de5cc4-3479-400a-9419-9f5024d6e389-76ABEB83E52A.local>
2026-05-06 14:59:56 +0000

    Updating Storyteller GAM Module to 11.3.3

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .upToNextMajor(from: "12.6.0")),
-        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "11.3.2")
+        .package(url: "https://github.com/getstoryteller/storyteller-sdk-swift-package", exact: "11.3.3")
     ],
     targets: [
         .binaryTarget(name: "StorytellerGAMIntegration",
-                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.3.2/StorytellerGAMIntegration.zip",
-                      checksum: "c1cb3f828a43937ef2b1c3cc46e99acf307fde8054bda0200884340577667311"),
+                      url: "https://storyteller.azureedge.net/sdk-ios/xcframeworks/11.3.3/StorytellerGAMIntegration.zip",
+                      checksum: "ab5fc270b7dcc4f4fa5b0b9467fda494840f0a991515efcadd28a432acf78749"),
         .target(
             name: "StorytellerGAMTarget",
             dependencies: [
